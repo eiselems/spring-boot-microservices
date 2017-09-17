edc89093be798d0c385ce6c8b4745a5a6c1673f4
Marcus Eisele <marcus.eisele@gmail.com>
2017-09-18 00:43:48 +0200

    Maint: Replace long with AtomicLong to make CounterController thread-safe
    Also add .gitignore

--- counterservice/src/main/java/com/marcuseisele/springboot_microservices/counterservice/api/CounterController.java
+++ counterservice/src/main/java/com/marcuseisele/springboot_microservices/counterservice/api/CounterController.java
@@ -4,13 +4,15 @@ package com.marcuseisele.springboot_microservices.counterservice.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class CounterController {
 
-    private static long count = 0L;
+    private static AtomicLong count = new AtomicLong(0L);
 
     @GetMapping(path = "/count")
     public long getCount(){
-        return count++;
+        return count.getAndIncrement();
     }
 }
