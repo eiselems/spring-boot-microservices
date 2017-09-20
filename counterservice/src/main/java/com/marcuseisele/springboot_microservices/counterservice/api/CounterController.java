e75e6d1105aaad9aaf2ad7430e3fa99ea3e950e7
Marcus Eisele <marcus.eisele@gmail.com>
2017-09-20 22:36:55 +0200

    Add part4 - ConfigService
    Also updates the counterService to leverage the configservice

--- counterservice/src/main/java/com/marcuseisele/springboot_microservices/counterservice/api/CounterController.java
+++ counterservice/src/main/java/com/marcuseisele/springboot_microservices/counterservice/api/CounterController.java
@@ -1,18 +1,24 @@
 package com.marcuseisele.springboot_microservices.counterservice.api;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RefreshScope
 public class CounterController {
 
-    private static AtomicLong count = new AtomicLong(0L);
+    private AtomicLong count = new AtomicLong(0L);
+
+    @Value(value = "${counter.prefixMessage}")
+    private String prefixMessage;
 
     @GetMapping(path = "/count")
-    public long getCount(){
-        return count.getAndIncrement();
+    public String getCount(){
+        return prefixMessage + count.getAndIncrement();
     }
 }
