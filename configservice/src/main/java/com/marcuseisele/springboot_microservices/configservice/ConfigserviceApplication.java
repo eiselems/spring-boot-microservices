385adeb11edf193cfde21075929dac0e1593ce52
Marcus Eisele <marcus.eisele@gmail.com>
2017-10-10 20:53:47 +0200

    Part5: WIP

--- configservice/src/main/java/com/marcuseisele/springboot_microservices/configservice/ConfigserviceApplication.java
+++ configservice/src/main/java/com/marcuseisele/springboot_microservices/configservice/ConfigserviceApplication.java
@@ -2,10 +2,12 @@ package com.marcuseisele.springboot_microservices.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigserviceApplication {
 
 	public static void main(String[] args) {
