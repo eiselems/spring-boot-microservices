521921d39746a0a6acd1581d67cb111f149d0d77
Marcus Eisele <marcus.eisele@gmail.com>
2017-10-19 21:59:44 +0200

    Part 6: Add zuul (#4)

---
+++ gatewayservice/src/test/java/com/marcuseisele/springboot_microservices/gateway/GatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marcuseisele.springboot_microservices.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
