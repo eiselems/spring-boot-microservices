e704e604a0e8c0ded24432ba898c2b4bc416d8e5
Marcus Eisele <marcus.eisele@gmail.com>
2017-10-19 21:58:06 +0200

    Part 6: Add zuul

---
+++ gatewayservice/src/main/java/com/marcuseisele/springboot_microservices/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.marcuseisele.springboot_microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
