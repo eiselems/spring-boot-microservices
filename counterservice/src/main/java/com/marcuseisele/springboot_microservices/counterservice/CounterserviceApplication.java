076bb26bde7d77fec2200ac74eaf68780b67f6ea
Marcus Eisele <marcus.eisele@gmail.com>
2018-05-14 18:07:13 +0200

    MAINT: Fix Docker by adjusting application.properties

--- counterservice/src/main/java/com/marcuseisele/springboot_microservices/counterservice/CounterserviceApplication.java
+++ counterservice/src/main/java/com/marcuseisele/springboot_microservices/counterservice/CounterserviceApplication.java
@@ -3,9 +3,11 @@ package com.marcuseisele.springboot_microservices.counterservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRetry
 public class CounterserviceApplication {
 
 	public static void main(String[] args) {
